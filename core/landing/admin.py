65031d2c4d5d79ef552177633bba5bc02eb876a4
DiegoBuencuero <diegobuencuero4095@gmail.com>
2024-11-07 19:05:02 -0300

    commist

--- core/landing/admin.py
+++ core/landing/admin.py
@@ -5,7 +5,7 @@ from .models import Atleta, DatosCondicion
 class DatosCondicionInline(admin.TabularInline):
     model = DatosCondicion
     extra = 1
-    readonly_fields = ('fecha_medicion', 'imc')  # Hace que `imc` y `fecha_medicion` sean solo lectura
+    readonly_fields = ('fecha_medicion', 'imc')  
     fields = ('fecha_medicion', 'peso', 'altura', 'circunferencia_cintura', 'circunferencia_cadera', 
               'nivel_condicion_fisica', 'objetivo_entrenamiento', 'historial_lesiones', 
               'frecuencia_entrenamiento', 'nivel_actividad_diaria', 'imc')
@@ -13,6 +13,5 @@ class AtletaAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'apellido', 'fecha_nacimiento', 'ciudad', 'email')
     search_fields = ('nombre', 'apellido')
     list_filter = ('ciudad',)
-    inlines = [DatosCondicionInline]  # Incluye el inline para mostrar los registros de condición física
-
+    inlines = [DatosCondicionInline]  
 admin.site.register(Atleta, AtletaAdmin)
